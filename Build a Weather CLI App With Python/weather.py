15edf5f30d319c55a7479adc0cfaf737d561b671
Megh <megh.shetty@veeva.com>
2022-05-31 09:05:13 +0100

    final code

--- Build a Weather CLI App With Python/weather.py
+++ Build a Weather CLI App With Python/weather.py
@@ -1,12 +1,14 @@
 # weather 
 import argparse
+from random import sample
 import ssl
 import json
 import sys
 from configparser import ConfigParser
 from urllib import  error, parse,request
+from pprint import pp # print data structures in a readable, pretty way
 
-ssl._create_default_https_context = ssl._create_unverified_context # choose not to authenticate SSL certificate
+#ssl._create_default_https_context = ssl._create_unverified_context # choose not to authenticate SSL certificate
 BASE_WEATHER_API_URL ="https://api.openweathermap.org/data/2.5/weather"  # base API URL as it is the same for all the API calls maked in this program 
 
 def _get_api_key():
@@ -47,24 +49,38 @@ def read_user_cli_args():
 def get_weather_data(query_url):
     try:
         response = request.urlopen(query_url)
-    except error.HTTPError:
-        print(error)
-        if error.HTTPError == 401:  # 401 - Unauthorized
+    except error.HTTPError as http_error:
+        if http_error.code == 401:  # 401 - Unauthorized
             sys.exit("Access denied. Check your API Key.")
-        elif error.HTTPError == 404:  # 404 - Not Found
+        elif http_error.code == 404:  # 404 - Not Found
             sys.exit("can't find weather data for this city")
         else:
-           sys.exit(f"something went wrong...({error.HTTPError})")
+           sys.exit(f"something went wrong...({http_error.code})")
     
     data = response.read()
     try:
         return json.loads(data)
     except json.JSONDecodeError:
         sys.exit("Couldn't read the server response.")
+
+def display_weather_info(weather_data, imperial=False):
+    city = weather_data ["name"]
+    weather_description = weather_data["weather"][0]["description"]
+    temperature = weather_data["main"]["temp"]
+
+    print (f"{city}", end="")
+    print(f"\t{weather_description.capitalize()}", end="")
+    print(f"({temperature}°{'F' if imperial else 'C'})")
   
 
 if __name__ == "__main__": # opens a conditional block after checking for Python’s "__main__" namespace, which allows you to define code that should run when you’re executing weather.py as a script
     user_args = read_user_cli_args()
     query_url = build_weather_query(user_args.city,user_args.imperial)
     weather_data = get_weather_data(query_url)
-    print(weather_data)
+    display_weather_info(weather_data, user_args.imperial)
+    #pp(weather_data)
+    #print(
+    #    f"{weather_data['name']}: "
+    #    f"{weather_data['weather'][0]['description']}"
+    #    f"({weather_data ['main']['temp']})"
+    #)
